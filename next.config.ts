f12e256afc3259fae2a706c638c7511bedb32099
kurt-constructs <kurtconst.ructs@gmail.com>
2025-09-10 00:13:13 +0800

    added config b2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     domains: [
       'firebasestorage.googleapis.com', // Firebase Storage domain
-      // add any other domains that your images are hosted on
+      'f004.backblazeb2.com', // Backblaze B2 domain
     ],
   },
 };
